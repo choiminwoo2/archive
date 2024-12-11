bea848b27b86d4aad4da308812cf62a4b499c6fd
ruu <wjrdjs005@naver.com>
2024-12-11 22:46:52 +0900

    ReactWeb Flux,mono 공부시작

---
+++ Java/reactive-alarm-bot/src/main/java/org/ruu/reactivealarmbot/ReactiveAlarmBotApplication.java
@@ -0,0 +1,13 @@
+package org.ruu.reactivealarmbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveAlarmBotApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReactiveAlarmBotApplication.class, args);
+  }
+
+}
---
+++ Java/reactive-alarm-bot/src/test/java/org/ruu/reactivealarmbot/ReactiveAlarmBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ruu.reactivealarmbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveAlarmBotApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
