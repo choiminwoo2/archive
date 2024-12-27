a7c8e4403e8933b7d19614420020b872ae732350
ruu <wjrdjs005@naver.com>
2024-12-27 23:50:07 +0900

    [ 코틀린 스프링 공부용 프로젝트 추가]

---
+++ Java/KotlinSpring/src/test/kotlin/org/ruu/kotlinspring/KotlinSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ruu.kotlinspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
